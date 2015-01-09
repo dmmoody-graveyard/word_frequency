f2d7046dcb4f5d1f16d0d86bf5beac67dfafe612
Duane M Moody <dmmoody@gmail.com>
2015-01-09 10:59:48 -0800

    add phrase spec for punctuation

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('word_frequency')
 
 describe('String#word_frequency') do
-  it('takes a letter as an object and returns the number of times it appears in a string provided as the argument') do
+  it("takes a string as an object and takes a word as an argument and returns how many occurances of the word") do
     expect('The dog ate dog food during the dog days of summer'.phrase?('dog')).to eq(3)
+  end
+
+  it('removes punctuation in the string passed as the object') do
+    expect('Oh. Wow! I, ate to much? wow.'.phrase?('wow')).to eq(2)
   end
 end
